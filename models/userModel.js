641e242de4ce828030dda87ba93d147bebaa4486
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-10-13 23:34:56 +0530

    register part done

--- models/userModel.js
+++ models/userModel.js
@@ -16,10 +16,9 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    type: {
-      type: String,
-      default: "user",
-      required: true,
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
     password: {
       type: String,
